0dbbc292e25862df7f7726e7c2da42175333e82f
Cedric Blanchard <the_atthis@proton.me>
2024-11-06 11:10:55 +0100

    affichage de l'application react lors de l'accès à la racine de l'url

--- back/server.js
+++ back/server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3615
+const path = require('path')
 
 const sqliteDB = require('./src/db/db.js');
 
+app.use(express.static(path.join(__dirname, 'dist')))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile('index.html')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
+  console.log(`Back de l'appication en fonctionnement sur l'url http://localhost:${port}`)
 })
