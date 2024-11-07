1e39f15d39d2933b163758305bf01682368ede5f
Cedric Blanchard <the_atthis@proton.me>
2024-11-07 18:42:12 +0100

    création d'un fichier de modele de ticket

--- back/src/api/models/receiptModel.js
+++ back/src/api/models/receiptModel.js
@@ -27,6 +27,35 @@ async function getReceipts(db) {
   return receipts;
 }
 
+async function getReceiptByUUID(db, uuid) {
+  const sql = 'SELECT * FROM receipts WHERE uuid = ?';
+
+  const rawData = await new Promise(function (resolve, reject) {
+    db.all(sql, [uuid], function (err, rows) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+
+  const receipts = [];
+
+  rawData.forEach(row => {
+    const jsonArticles = JSON.parse(row.articles_data);
+
+    const formatedRow = {
+      ...row,
+      articles_data: jsonArticles
+    };
+
+    receipts.push(formatedRow);
+  })
+
+  return receipts;
+}
+
 module.exports = {
   getReceipts
 }
