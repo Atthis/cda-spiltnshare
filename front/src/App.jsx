283f47ec976fa5c5abda50b7884e1618ef6b9e22
FnnyJsst <fanny.jousset@gmail.com>
2024-11-07 18:25:36 +0100

    Creation de la page d'edition

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Logo from '../components/Logo';
-import BreadCrumb from '../components/BreadCrumb';
-import BillUploader from '../screens/BillUploader';
-import BillViewer from '../screens/BillViewer';
+import Logo from './components/Logo';
+import BreadCrumb from './components/BreadCrumb';
+import BillUploader from './screens/BillUploader';
+import BillViewer from './screens/BillViewer';
+import BillEditor from './screens/BillEditor';
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="" element={<BillUploader setSelectedFile={setSelectedFile} />} />
         <Route path="/view-file" element={<BillViewer file={selectedFile} />} />
+        <Route path="/edit-file" element={<BillEditor file={selectedFile} />} />
       </Routes>
     </Router>
   );
