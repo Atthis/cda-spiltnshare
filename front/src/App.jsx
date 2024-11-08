7ce881e578eb34ece42be18bcac99e863e9181c2
EliottWCRT <eliott.creative@gmail.com>
2024-11-08 14:00:09 +0100

    modification deuxieme page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -11,12 +11,17 @@ function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [OCRData, setOCRData] = useState(null);
 
+  const [selectedView, setSelectedView] = useState('scanner'); // Initialiser avec 'scanner'
+
   return (
     <Router>
       <Logo />
-      <BreadCrumb />
+      <BreadCrumb selectedView={selectedView} setSelectedView={setSelectedView} />
       <Routes>
-        <Route path="" element={<BillUploader setSelectedFile={setSelectedFile} />} />
+      <Route 
+          path="" 
+          element={<BillUploader setSelectedFile={setSelectedFile} selectedView={selectedView} />} 
+        />
         <Route path="/view-file" element={<BillViewer file={selectedFile} ocrData={{OCRData, setOCRData}}/>} />
         <Route path="/edit-file" element={<BillEditor file={selectedFile} ocrData={{OCRData, setOCRData}}/>} />
       </Routes>
